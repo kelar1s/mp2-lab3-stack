88e33044b4c1aad30c86f8fb9c4d957b3d013408
kelar1s <atabalaev21@gmail.com>
2024-12-13 23:55:36 +0300

    finished stack using list

--- TStack.cpp
+++ TStack.cpp
@@ -1,179 +1,150 @@
 template <class T>
 struct TNode {
-	T val;
-	TNode* next = nullptr;
+    T val;
+    TNode* next = nullptr;
 public:
-	TNode() {
-		val = 0;
-	}
-	TNode(int value) {
-		val = value;
-	}
+    TNode() : val(T()) {}
+    TNode(T value) : val(value) {}
 };
 
 template <class T>
 class TStack {
-	TNode<T>* pFirst;
+    TNode<T>* pFirst;
 public:
-	TStack();
-	TStack(const TStack& oth);
-	~TStack();
+    TStack();
+    TStack(const TStack& oth);
+    ~TStack();
 
-	bool operator==(const TStack& oth) const;
-	bool operator!=(const TStack& oth) const;
-	TStack<T>& operator=(const TStack& oth);
+    bool operator==(const TStack& oth) const;
+    bool operator!=(const TStack& oth) const;
+    TStack<T>& operator=(const TStack& oth);
 
-	bool isEmpty() { return pFirst == nullptr; }
-	bool isFull();
+    bool isEmpty() const { return pFirst == nullptr; }
+    bool isFull() const;
 
-	void Clear();
-	
-	T Top();
-	T Pop();
-	void Push(T value);
+    void Clear();
+
+    T Top() const;
+    T Pop();
+    void Push(T value);
 };
 
 template <class T>
-TStack<T>::TStack() {
-	pFirst = nullptr;
-}
+TStack<T>::TStack() : pFirst(nullptr) {}
 
 template <class T>
-TStack<T>::TStack(const TStack& oth) {
-	TNode<T>* tmp = oth.pFirst;
-	pFirst = new TNode<T>;
-	pFirst->val = oth.pFirst->val;
-	pFirst->next = nullptr;
-	TNode<T>* prev = pFirst;
-	tmp = tmp->next;
-	while (tmp != nullptr) {
-		TNode<T>* node = new TNode<T>;
-		node->val = tmp->val;
-		node->next = nullptr;
-		prev->next = node;
-		tmp = tmp->next;
-		prev = prev->next;
-	}
+TStack<T>::TStack(const TStack& oth) : pFirst(nullptr) {
+    if (oth.pFirst == nullptr) {
+        return;
+    }
+    pFirst = new TNode<T>(oth.pFirst->val);
+    TNode<T>* tmp = oth.pFirst->next;
+    TNode<T>* prev = pFirst;
+    while (tmp != nullptr) {
+        TNode<T>* node = new TNode<T>(tmp->val);
+        prev->next = node;
+        tmp = tmp->next;
+        prev = prev->next;
+    }
 }
 
 template <class T>
 TStack<T>::~TStack() {
-	TNode<T>* p;
-	while (pFirst != nullptr) {
-		p = pFirst;
-		pFirst = pFirst->next;
-		delete p;
-	}
+    Clear();
 }
 
 template <class T>
 bool TStack<T>::operator==(const TStack& oth) const {
-	if (this == *oth) {
-		return true;
-	}
-	if (this->pFirst == nullptr && oth->pFirst == nullptr) {
-		return true;
-	}
-	TNode<T>* tmp1 = pFirst, tmp2 = oth.pFirst;
-	while (tmp1 != nullptr && tmp2 != nullptr) {
-		if (tmp1->val != tmp2->val) {
-			return false;
-		}
-		tmp1 = tmp1->next;
-		tmp2 = tmp2->next;
-	}
-	if (tmp1 == tmp2) {
-		return true;
-	}
-	return false;
+    if (this == &oth) {
+        return true;
+    }
+    TNode<T>* tmp1 = pFirst;
+    TNode<T>* tmp2 = oth.pFirst;
+    while (tmp1 != nullptr && tmp2 != nullptr) {
+        if (tmp1->val != tmp2->val) {
+            return false;
+        }
+        tmp1 = tmp1->next;
+        tmp2 = tmp2->next;
+    }
+    return tmp1 == tmp2;
 }
 
 template <class T>
 bool TStack<T>::operator!=(const TStack& oth) const {
-	return !(*this == oth);
+    return !(*this == oth);
 }
 
 template <class T>
 TStack<T>& TStack<T>::operator=(const TStack& oth) {
-	if (this == &oth) {
-		return *this;
-	}
-	if (pFirst != nullptr) {
-		TNode<T>* p;
-		while (pFirst != nullptr) {
-			p = pFirst;
-			pFirst = pFirst->next;
-			delete p;
-		}
-	}
-	TNode<T>* tmp = oth.pFirst;
-	pFirst = new TNode<T>;
-	pFirst->val = oth.pFirst->val;
-	pFirst->next = nullptr;
-	TNode<T>* prev = pFirst;
-	tmp = tmp->next;
-	while (tmp != nullptr) {
-		TNode<T>* node = new TNode<T>;
-		node->val = tmp->val;
-		node->next = nullptr;
-		prev->next = node;
-		tmp = tmp->next;
-		prev = prev->next;
-	}
-	return *this;
+    if (this == &oth) {
+        return *this;
+    }
+    Clear();
+    if (oth.pFirst == nullptr) {
+        pFirst = nullptr;
+        return *this;
+    }
+    pFirst = new TNode<T>(oth.pFirst->val);
+    TNode<T>* tmp = oth.pFirst->next;
+    TNode<T>* prev = pFirst;
+    while (tmp != nullptr) {
+        TNode<T>* node = new TNode<T>(tmp->val);
+        prev->next = node;
+        tmp = tmp->next;
+        prev = prev->next;
+    }
+    return *this;
 }
 
 template <class T>
-bool TStack<T>::isFull() {
-	TNode<T>* tmp;
-	try {
-		tmp = new TNode<T>;
-	}
-	catch (...) {
-		return true;
-	}
-	delete tmp;
-	return false;
+bool TStack<T>::isFull() const {
+    try {
+        TNode<T>* tmp = new TNode<T>();
+        delete tmp;
+        return false;
+    }
+    catch (...) {
+        return true;
+    }
 }
 
 template <class T>
 void TStack<T>::Clear() {
-	TNode<T>* p;
-	while (pFirst != nullptr) {
-		p = pFirst;
-		pFirst = pFirst->next;
-		delete p;
-	}
+    while (pFirst != nullptr) {
+        TNode<T>* p = pFirst;
+        pFirst = pFirst->next;
+        delete p;
+    }
 }
 
 template <class T>
-T TStack<T>::Top() {
-	if (pFirst == nullptr) {
-		throw - 1;
-	}
-	return pFirst->val;
+T TStack<T>::Top() const {
+    if (pFirst == nullptr) {
+        throw -1;
+    }
+    return pFirst->val;
 }
 
 template <class T>
 T TStack<T>::Pop() {
-	if (this->isEmpty()) {
-		throw -1;
-	}
-	T res = pFirst->val;
-	TNode<T>* p = pFirst;
-	pFirst = pFirst->next;
-	delete p;
-	return res;
+    if (isEmpty()) {
+        throw -1;
+    }
+    T res = pFirst->val;
+    TNode<T>* p = pFirst;
+    pFirst = pFirst->next;
+    delete p;
+    return res;
 }
 
 template <class T>
 void TStack<T>::Push(T value) {
-	if (this->isFull()) {
-		throw -1;
-	}
-	TNode<T>* tmp = new TNode<T>;
-	tmp->val = value;
-	tmp->next = pFirst;
-	pFirst = tmp;
+    if (isFull()) {
+        throw -1;
+    }
+    TNode<T>* tmp = new TNode<T>(value);
+    tmp->next = pFirst;
+    pFirst = tmp;
 }
 
